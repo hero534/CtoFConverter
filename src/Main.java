37801d60247ba9b128cfbafacca27769e2eb6ad3
hgold <goldsmhr@mail.uc.edu>
2025-02-16 11:33:37 -0500

    Task 01 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        double tempC = 0.00;
+        double tempF = 0.00;
+        String trash = "";
+        boolean done = false;
+            do {
+                System.out.println("Enter the temperature in Celsius: ");
+                    if (in.hasNextDouble()) {
+                        tempC = in.nextDouble();
+                        tempF = tempC * 1.8 + 32;
+                        done = true;
+                        System.out.println("Given " + tempC + "your temperature in Fahrenheit is: " + tempF);
+                    }
+                    else {
+                        trash = in.nextLine();
+                        System.out.println("\nYou said your temperature was: " + trash);
+                    }
+            }while(!done);
     }
 }
